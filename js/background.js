34636cecf78ba80594f7fecffbbe67bf6cd05617
entriol <entriol@outlook.com>
2015-11-28 13:20:09 +0200

    contextMenu and options page to toggle it

---
+++ js/background.js
@@ -0,0 +1,18 @@
+chrome.storage.sync.get({
+	contextMenu: false
+}, function(items) {
+	if (items.contextMenu) createContextMenu();
+	console.log(items);
+});
+  
+function createContextMenu() {
+	chrome.contextMenus.create({
+		title: "Multi Sözlük: %s", 
+		contexts: ["selection"], 
+		onclick: function(info, tab){ 
+			chrome.tabs.create(
+				{"url" : "chrome-extension://" + chrome.runtime.id + "/popup.html#" + info.selectionText }
+			);
+		}
+	});
+}
--- js/options.js
+++ js/options.js
@@ -1,3 +1,33 @@
 $(document).foundation();
 
 $('h2 small').html(chrome.runtime.getManifest().version);
+
+// Saves options to chrome.storage
+function save_options() {
+  var contextMenu = document.getElementById('contextMenu').checked;
+  chrome.storage.sync.set({
+    contextMenu: contextMenu
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Kaydedildi.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value
+  chrome.storage.sync.get({
+    contextMenu: false
+  }, function(items) {
+    document.getElementById('contextMenu').checked = items.contextMenu;
+    console.log(items);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+$('#contextMenu').on('change', save_options);
